ddf76e048768544c3aabefadd779811d2486c350
shino16 <shinokawa.m+git@gmail.com>
2024-11-21 03:58:58 +0000

    Make SaturatingArith.hpp shorter

--- content/various/shino16/SaturatingArith.hpp
+++ content/various/shino16/SaturatingArith.hpp
@@ -4,18 +4,8 @@
  * the range that the type can represent. Use LLONG\_MIN/LLONG\_MAX for long longs.
  */
 
-auto sat_add(int a, int b) {
+auto sat_[add|sub|mul](int a, int b) {
   int res;
-  return __builtin_add_overflow(a, b, &res) ?
-                      (a < 0 ? INT_MIN : INT_MAX) : res;
-}
-auto sat_sub(int a, int b) {
-  int res;
-  return __builtin_sub_overflow(a, b, &res) ?
-                      (a < 0 ? INT_MIN : INT_MAX) : res;
-}
-auto sat_mul(int a, int b) {
-  int res;
-  return __builtin_mul_overflow(a, b, &res) ?
-          ((a < 0) == (b < 0) ? INT_MAX : INT_MIN) : res;
+  return __builtin_[add|sub|mul]_overflow(a, b, &res) ?
+    (a<0 ? INT_MIN : INT_MAX) : res; // (a<0) != (b<0) for mul
 }
