d7b3fbc23f04f967fe29a08b51967d247d48e827
shino16 <shinokawa.m+git@gmail.com>
2024-11-21 03:18:48 +0000

    Tidy up my stuff

--- content/various/shino16/Div.hpp
+++ content/various/shino16/Div.hpp
@@ -3,15 +3,12 @@
  * Description: Division with rounding towards -inf (floor) or inf (ceil).
  */
 
-template <class T>
-T div_floor(T a, T b) {
-  T q = a / b, r = a % b;
+int div_floor(int a, int b) {
+  int q = a / b, r = a % b;
   return q - ((r > 0 && b < 0) || (r < 0 && b > 0));
 }
-template <class T>
-T div_ceil(T a, T b) {
-  T q = a / b, r = a % b;
+int div_ceil(int a, int b) {
+  int q = a / b, r = a % b;
   return q + ((r > 0 && b > 0) || (r < 0 && b < 0));
 }
-template <class T>
-T rem(T a, T b){ T r = a % b; return r >= 0 ? r : r + abs(b); }
+int rem(int a, int b){ int r = a % b; return r >= 0 ? r : r + abs(b); }
